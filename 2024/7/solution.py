515e33055ba1ab6e41a8391f02969f5a1c0963cd
Jeremy Waller <jeremyw@phase2online.com>
2024-12-10 12:32:43 -0600

    Day 7 Part 2

--- 2024/7/solution.py
+++ 2024/7/solution.py
@@ -1,10 +1,3 @@
-ALLOWED_OPERATIONS = ["+", "*"]
-
-OPERATORS = {
-    "+": lambda x, y: x + y,
-    "*": lambda x, y: x * y
-}
-
 def parse_data(file_path):
     with open(file_path, 'r') as file:
         data = file.read()
@@ -19,25 +12,30 @@ def parse_data(file_path):
         })
     return data_set
 
-def evaluate_equation(answer, equation):
+def evaluate_equation(answer, equation, include_concat=False):
+    equation = [int(x) for x in equation]
     if equation[0] > answer:
         return False
     
     if len(equation) == 1:
         return equation[0] == answer
     
-    if(evaluate_equation(answer, [equation[0] + equation[1]] + equation[2:])):
+    if(evaluate_equation(answer, [equation[0] + equation[1]] + equation[2:], include_concat)):
         return True
-    if(evaluate_equation(answer, [equation[0] * equation[1]] + equation[2:])):
+    if(evaluate_equation(answer, [equation[0] * equation[1]] + equation[2:], include_concat)):
+        return True
+    if(include_concat and evaluate_equation(answer, [str(equation[0]) + str(equation[1])] + equation[2:], include_concat)):
         return True
     return False
 
 def main():
     data = parse_data("7/input.txt")
     total = len(data)
-    results = sum(item["answer"] for item in data if evaluate_equation(item["answer"], item["equation"]))
+    part1_results = sum(item["answer"] for item in data if evaluate_equation(item["answer"], item["equation"]))
+    part2_results = sum(item["answer"] for item in data if evaluate_equation(item["answer"], item["equation"], True))
     
-    print(f'Part 1: {results}')
+    print(f'Part 1: {part1_results}')
+    print(f'Part 2: {part2_results}') 
 
 if __name__ == "__main__":
     main()
--- 2024/7/test.py
+++ 2024/7/test.py
@@ -3,9 +3,13 @@ import unittest
 from solution import parse_data, evaluate_equation
 
 class TestAdventDay7(unittest.TestCase):
-    def test_evaluate_equation(self):
+    def test_evaluate_equation_part1(self):
         data = parse_data("7/test_input.txt")
         self.assertEqual(sum(int(item["answer"]) for item in data if evaluate_equation(item["answer"], item["equation"])), 3749, "Expected 3,749")
+
+    def test_evaluate_equation_part2(self):
+        data = parse_data("7/test_input.txt")
+        self.assertEqual(sum(int(item["answer"]) for item in data if evaluate_equation(item["answer"], item["equation"], True)), 11387, "Expected 11,387")
 
 def main():
     suite = unittest.TestLoader().loadTestsFromTestCase(TestAdventDay7)
