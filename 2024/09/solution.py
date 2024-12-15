9ba96f50cb1a47a9375b77af6dc2ffc5cc534ee5
Jeremy Waller <jeremyw@phase2online.com>
2024-12-14 23:08:39 -0600

    Day 9 rework w/ part 2

--- 2024/09/solution.py
+++ 2024/09/solution.py
@@ -1,63 +1,94 @@
-def parse_data(file_path):
-    with open(file_path, "r") as file:
-        return file.read()
+class DiskDefragmenter:
+    def __init__(self, disk_map):
+        self.disk_map = disk_map
+        self.blocks = []
+        self.parse_disk_map()
 
-def parse_disk_map(disk_map):
-    # disk_details:
-    # (id, original_position, is_file)
-    disk_details = []
-    file_id = 0
-    free_space_id = 0
-    for idx, size in enumerate(disk_map):
-        if idx % 2 == 0:
-            # is file
-            for i in range(int(size)):
-                disk_details.append((file_id, len(disk_details), True))
-            file_id += 1
-        else:
-            # is free space
-            for i in range(int(size)):
-                disk_details.append((free_space_id, len(disk_details), False))
-            free_space_id += 1
-    return disk_details
-
-def find_next_open_space(disk_details, start_pos=0):
-    for idx in range(start_pos, len(disk_details)):
-        if not disk_details[idx][2]:  # not is_file
-            return idx
-    return -1
-
-def visualize_disk(disk_details):
-    disk_str = ""
-    for id, position, is_file in disk_details:
-        if is_file:
-            disk_str += str(id)
-        else:
-            disk_str += "."
-    return disk_str
-
-def defragment(disk_details):
-    files = [(id, pos) for id, pos, is_file in disk_details if is_file]
-    files.sort(key=lambda x: (x[0], x[1]), reverse=True)
+    def parse_disk_map(self):
+        file_id = 0
+        for i, length in enumerate(self.disk_map):
+            length = int(length)
+            if i % 2 == 0:  # File blocks
+                self.blocks.extend([file_id] * length)
+                file_id += 1
+            else:  # Free space blocks
+                self.blocks.extend(['.'] * length)
     
-    last_open_space = 0
-    for file_id, orig_pos in files:
-        open_space = find_next_open_space(disk_details, last_open_space)
-        if open_space < orig_pos:
-            disk_details[open_space] = (file_id, orig_pos, True)
-            if open_space != orig_pos:
-                disk_details[orig_pos] = (-1, -1, False)
-            last_open_space = open_space + 1
+    def print_state(self):
+        print(''.join(str(x) if x != '.' else '.' for x in self.blocks))
+    
+    def compact_part1(self):
+        moved = True
+        while moved:
+            moved = False
+            # Find rightmost file block
+            for i in range(len(self.blocks)-1, -1, -1):
+                if self.blocks[i] != '.':
+                    # Find leftmost free space
+                    for j in range(i):
+                        if self.blocks[j] == '.':
+                            # Move block
+                            self.blocks[j] = self.blocks[i]
+                            self.blocks[i] = '.'
+                            moved = True
+                            break
+                    break
+                            
+    def compact_part2(self):
+        # Get unique file IDs and sort in descending order
+        file_ids = sorted(set(x for x in self.blocks if x != '.'), reverse=True)
+        
+        for file_id in file_ids:
+            # Get file size and current positions
+            positions = [i for i, x in enumerate(self.blocks) if x == file_id]
+            file_size = len(positions)
+            if not positions:
+                continue
+                
+            # Find leftmost valid position for the whole file
+            best_start = None
+            free_count = 0
+            last_good_start = None
+            
+            for i in range(min(positions)):
+                if self.blocks[i] == '.':
+                    if free_count == 0:
+                        last_good_start = i
+                    free_count += 1
+                    if free_count >= file_size:
+                        best_start = last_good_start
+                        break
+                else:
+                    free_count = 0
+                    
+            # If we found a valid position, move the file
+            if best_start is not None:
+                # Clear old positions
+                for pos in positions:
+                    self.blocks[pos] = '.'
+                # Place file in new position
+                for i in range(file_size):
+                    self.blocks[best_start + i] = file_id
+            
+    def calculate_checksum(self):
+        return sum(pos * block for pos, block in enumerate(self.blocks) 
+                  if block != '.')
 
-def calculate_checksum(disk_details):
-    #print(disk_details)
-    return sum(idx * id for idx, (id, position, is_file) in enumerate(disk_details) if is_file)
+def solve_part1(disk_map):
+    defrag = DiskDefragmenter(disk_map)
+    defrag.compact_part1()
+    return defrag.calculate_checksum()
+
+def solve_part2(disk_map):
+    defrag = DiskDefragmenter(disk_map)
+    defrag.compact_part2()
+    return defrag.calculate_checksum()
 
 def main():
-    disk_map = parse_data("09/input.txt")
-    disk_details = parse_disk_map(disk_map)
-    defragment(disk_details)
-    print(calculate_checksum(disk_details))
+    with open("09/input.txt", "r") as file:
+        disk_map = file.read()
+    print(f"Part 1 solution: {solve_part1(disk_map)}")
+    print(f"Part 2 solution: {solve_part2(disk_map)}")
 
 if __name__ == "__main__":
     main()
